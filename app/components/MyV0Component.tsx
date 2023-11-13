24f8cfa602d8c4a21166413bc88f6683bdc3ba93
davevercel <101666938+davevercel@users.noreply.github.com>
2023-11-13 10:56:44 -0500

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,20 +1,23 @@
 /**
  * v0 by Vercel.
- * @see https://v0.dev/t/LdxmWh9G5SG
+ * @see https://v0.dev/t/lzZqaH8Hp2P
  */
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function Component() {
   return (
-    <div className="flex flex-col items-center justify-center w-full p-4">
-      <h1 className="text-2xl font-semibold mb-4">List Your Top 5 Favorite Movies</h1>
+    <div className="flex flex-col items-center justify-center w-full p-4 bg-black text-white">
+      <h1 className="text-2xl font-semibold mb-4">List Your Top 10 Favorite Movies</h1>
       <form className="w-full flex flex-col items-center justify-center mb-8">
-        <Button className="mt-4" variant="default">
+        <Button className="mt-4 bg-white text-black" variant="default">
           Submit
         </Button>
       </form>
-      <h2 className="text-xl font-semibold mb-4">Your List</h2>
-      <div className="flex flex-wrap justify-center gap-4" />
+      <div className="w-full max-w-md my-2">
+        <Input className="pl-2 w-full bg-white text-black" placeholder="Your List..." type="text" />
+      </div>
+      <ol className="list-decimal list-inside text-left w-full max-w-md bg-white text-black p-4 rounded" />
     </div>
   )
 }
